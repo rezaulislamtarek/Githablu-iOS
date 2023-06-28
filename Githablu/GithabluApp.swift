f40b635b86e0e290c2c715e89451bca48ae72ca2
rezaulislamtarek <mrezaulislam.t@gmail.com>
2023-06-28 19:22:56 +0600

    Initial Commit

---
+++ Githablu/GithabluApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithabluApp.swift
+//  Githablu
+//
+//  Created by Rezaul Islam on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GithabluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
